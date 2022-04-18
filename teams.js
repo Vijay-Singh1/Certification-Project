da78e1aead36d1a52dba2eeb3fea92b93487f827
Vijay-Singh1 <98080231+Vijay-Singh1@users.noreply.github.com>
2022-04-18 18:58:21 +0530

    Update teams.js

--- teams.js
+++ teams.js
@@ -3,43 +3,43 @@
 
 
 
- $.get(`details.json`, function abc(response) {
-    var productData = response
-    ab(productData)
+
+var n="rcb"
+
+$.get(`details.json`, function abc(response) {
+    const obj = response;
+
+  teamscard(obj[n])
+
+
+   
 
 
 })
 
-
-function ab(m){
-   console.log(m.rcb[0].name)
-
+function teamscard(res){
    
-        }
-
-    
+    for(i=0;i<=res.length-1;i++){
        
+    
 
-     
-     function card(name,img,from,status,price,des,res){
-        console.log(res)
-     
  
+   
      
-     
-     var a = document.getElementById("team-member")
-     a.innerHTML+=`<div class="teams-cards" id=${res}>
-     <img src=${img} alt="" class="img">
-     <h2 class="name">${name}</h2>
-     <h6 class="Player-count">${from}</h6>
-      <h2 class="Top-Batter">${status}</h2> 
-      <h2 class="Top-Bowler">${price}</h2> 
-      <h2 class="title-won">${des}</h2> 
+
+var a = document.getElementById("team-member")
+     a.innerHTML+=`<div class="teams-cards" >
+     <img src=${res[i].preview} alt="" class="img">
+     <h2 class="name">${res[i].name}</h2>
+     <h6 class="Player-count">${res[i].from}</h6>
+      <h2 class="Top-Batter">${res[i].status}</h2> 
+      <h2 class="Top-Bowler">${res[i].price}</h2> 
+      <h2 class="title-won">${res[i].des}</h2> 
       
      </div>`
-     
-     
-     
-     
-     }
+    }
+    }
     
+
+    
+
